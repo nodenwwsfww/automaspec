16a9f90d0bb3edf2a490bbe8e320dcb9375b563f
Roman Radchenko <nodenwwsfww@gmail.com>
2025-07-05 15:38:31 +0300

    [landing-page]: Remove "View Demo" button from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -78,15 +78,6 @@ export default function LandingPage() {
                 <ArrowRight className="ml-2 h-5 w-5" />
               </Button>
             </Link>
-            <Link href="#demo">
-              <Button
-                className="bg-transparent px-8 text-lg"
-                size="lg"
-                variant="outline"
-              >
-                View Demo
-              </Button>
-            </Link>
           </div>
         </div>
       </section>
