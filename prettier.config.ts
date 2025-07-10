0f4520441df1964b046827526b83dd50a27ac26e
Aliaksandr <grubian2@gmail.com>
2025-07-10 18:03:17 +0300

    add eslint and prettier

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+  };
+export default config;
