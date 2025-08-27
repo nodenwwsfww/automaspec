515bdae7879cb1c0bdc541cb9faefd47c5747042
Aliaksandr <grubian2@gmail.com>
2025-08-27 21:55:27 +0300

    ban ts errors

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         ignoreDuringBuilds: true
     },
     typescript: {
-        ignoreBuildErrors: true
+        ignoreBuildErrors: false
     },
     images: {
         unoptimized: true
